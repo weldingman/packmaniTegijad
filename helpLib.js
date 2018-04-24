e8add308a828a8013a6323da01dc2f32abb6ef3c
keevitajamees <sven.suiste@gmail.com>
2018-04-24 23:51:13 +0300

    create library with extra features, collision detection, key events and so on...

---
+++ helpLib.js
@@ -0,0 +1,111 @@
+var moving = {
+  u: false,
+  d: false,
+  r: false,
+  l: false,
+  s: false
+};
+
+var testLib = {
+
+
+  genRandEvent: function(prob){
+    prob = prob/100;
+    if(random() <= prob){
+      return true;
+    }
+    return false;
+  },
+
+  circCircColl: function(c1, c2){
+    var a = {
+      x:c1.x,
+      y:c1.y
+    }
+    var b = {
+      x:c2.x,
+      y:c2.y
+    }
+    var dist = this.calcDist(a, b);
+    if(dist <= c1.r / 2 + c2.r / 2){
+      return true;
+    }
+    return false;
+  },
+
+  calcDist: function(a, b){
+    var dist = sqrt(((b.x - a.x) * (b.x - a.x)) + ((b.y - a.y) * (b.y - a.y)));
+    return dist;
+  },
+
+  circRectCol: function(circle, rectIn, mode){
+    var rectA = {
+      x: rectIn.x,
+      y: rectIn.y,
+      w: rectIn.w,
+      h: rectIn.h
+    };
+    var tempR = circle.r;
+    if(mode){
+      var tempR = circle.r/2;
+    }
+    //rect(rectA.x, rectA.y, rectA.w, rectA.h);
+    //ellipse(circle.x, circle.y, circle.r * 2, circle.r * 2);
+    var circleDistance = {
+      x: abs(circle.x - rectA.x),
+      y: abs(circle.y - rectA.y)
+    };
+
+    // if distance along x is greater than half scuare dist and circle radius
+    // then it's deffinetly can't be colliding
+    if (circleDistance.x > (rectA.w/2 + tempR)) { return false; }
+    // if distance along y is greater than half scuare dist and circle radius
+    // then it's deffinetly can't be colliding
+    if (circleDistance.y > (rectA.h/2 + tempR)) { return false; }
+
+    // if it hasn't returned false check if circle is close inaf to rect
+    // check's for collision along y axis
+    if (circleDistance.x <= (rectA.w/2)) { return true; }
+    // check's for collision along x axis
+    if (circleDistance.y <= (rectA.h/2)) { return true; }
+
+    // checks for corner collisions
+    var cornerDistance_sq = Math.pow((circleDistance.x - rectA.w/2),2) +
+                         Math.pow((circleDistance.y - rectA.h/2),2);
+
+    return (cornerDistance_sq <= Math.pow(tempR,2));
+  },
+
+  rectRectCol: function(rectA, rectB){
+    return (rectA.x < rectB.x + rectB.w && rectA.x + rectA.w > rectB.x &&
+    rectA.y < rectB.y + rectB.h && rectA.y + rectA.h > rectB.y);
+  },
+
+  keys: function(){
+    document.onkeydown = function(event){
+      if(event.keyCode === 68)
+        moving.r = true;
+      else if(event.keyCode === 83)   //s
+    		moving.d = true;
+      else if(event.keyCode === 65) //a
+    		moving.l = true;
+      else if(event.keyCode === 87) // w
+    		moving.u = true;
+      else if(event.keyCode === 32) // space
+        moving.s = true;
+      }
+      document.onkeyup = function(event){
+    		if(event.keyCode === 68)    //d
+    			moving.r = false;
+    		else if(event.keyCode === 83)   //s
+    		  moving.d = false;
+    		else if(event.keyCode === 65) //a
+    			moving.l = false;
+    		else if(event.keyCode === 87) // w
+    			moving.u = false;
+        else if(event.keyCode === 32) // space
+          moving.s = false;
+        }
+        return moving;
+  }
+}
