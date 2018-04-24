cc1049b591e1b87c504cf6fa8a6ee0a063e584a4
keevitajamees <sven.suiste@gmail.com>
2018-04-25 00:14:10 +0300

    add move function

--- pacman.js
+++ pacman.js
@@ -14,7 +14,27 @@ class Pacman{
 
   show(){
     fill("yellow");
+    this.move();
     ellipse(this.x, this.y, this.w, this.h);
+  }
+
+  move(){
+    if(testLib.keys().r){
+      this.dir = "right";
+      this.x += this.speed;
+    }
+    if(testLib.keys().l){
+      this.dir = "left";
+      this.x -= this.speed;
+    }
+    if(testLib.keys().u){
+      this.dir = "up";
+      this.y -= this.speed;
+    }
+    if(testLib.keys().d){
+      this.dir = "down";
+      this.y += this.speed;
+    }
   }
 
 }
