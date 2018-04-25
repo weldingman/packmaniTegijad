9067696dc52ef4ba936e4531a84d7190752f671d
keevitajamees <sven.suiste@gmail.com>
2018-04-25 17:01:04 +0300

    update sketch

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 var pac;
+var world;
 
 function setup() {
 	createCanvas(640, 480);
-  pac = new Pacman({x:100, y:100}, {w:30, h:30}, 1, "up");
+  pac = new Pacman({x:100, y:100}, {w:40, h:40}, 5, "up");
+  world = new TestWorld({w:20,h:20});
 }
 
 function draw() {
 	background(0);
-  pac.update();
+  pac.update(world.update());
 }
