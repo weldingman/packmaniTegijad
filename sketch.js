0bbae8f5817dce96c5ad4f62f5d1292422ab507c
keevitajamees <sven.suiste@gmail.com>
2018-04-26 01:05:22 +0300

    add 1 ghost

--- sketch.js
+++ sketch.js
@@ -1,13 +1,16 @@
 var pac;
 var world;
+var ghost;
 
 function setup() {
 	createCanvas(640, 480);
   pac = new Pacman({x:100, y:100}, {w:40, h:40}, 5, "up");
+	ghost = new Ghost({x:200, y:200}, {w:40, h:40}, 5, "up");
   world = new TestWorld({w:20,h:20});
 }
 
 function draw() {
 	background(0);
-  pac.update(world.update());
+  pac.update(world.update().wall);
+	ghost.update(world.update(), pac);
 }
