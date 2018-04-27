e219813bffc0b63590daa1ad4136135ff57a5a23
keevitajamees <sven.suiste@gmail.com>
2018-04-27 21:18:29 +0300

    change framerate and speed

--- public/sketch.js
+++ public/sketch.js
@@ -18,9 +18,9 @@ function setup() {
 	//createCanvas(640, 480);
 	const canvasElt = createCanvas(640, 480).elt;
   canvasElt.style.width = '50%', canvasElt.style.height = '50%';
-  pac = new Pacman({x:100, y:100}, {w:40, h:40}, 20, "up");
+  pac = new Pacman({x:100, y:100}, {w:40, h:40}, 7, "up");
 	for(var i = 0; i < numOfGhosts; i++){
-		ghosts.push(new Ghost({x:200, y:200}, {w:40, h:40}, 15, "up"));
+		ghosts.push(new Ghost({x:200, y:200}, {w:40, h:40}, 6, "up"));
 	}
 	// ghost = new Ghost({x:200, y:200}, {w:40, h:40}, 5, "up");
 	// ghost2 = new Ghost({x:200, y:200}, {w:40, h:40}, 5, "up");
@@ -30,7 +30,7 @@ function setup() {
 
 function draw() {
 	background(0);
-	frameRate(20);
+	frameRate(30);
   pac.update(world.update().wall);
 	for(var i = 0; i < ghosts.length; i++){
 		if(ghosts[i].update(world.update(), pac)){
