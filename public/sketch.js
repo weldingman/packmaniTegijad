577b7c83169201a4ae250e291fc95a0624957e91
keevitajamees <sven.suiste@gmail.com>
2018-04-27 23:45:45 +0300

    change speeds

--- public/sketch.js
+++ public/sketch.js
@@ -20,9 +20,9 @@ function setup() {
 	createCanvas(640, 480);
 	//const canvasElt = createCanvas(640, 480).elt;
   //canvasElt.style.width = '50%', canvasElt.style.height = '50%';
-  pac = new Pacman({x:30, y:30}, {w:20, h:20}, 3, "up");
+  pac = new Pacman({x:30, y:30}, {w:20, h:20}, 4, "up");
 	for(var i = 0; i < numOfGhosts; i++){
-		ghosts.push(new Ghost({x:280, y:190}, {w:20, h:20}, 2, "up"));
+		ghosts.push(new Ghost({x:280, y:190}, {w:20, h:20}, 4, "up"));
 	}
 	// ghost = new Ghost({x:200, y:200}, {w:40, h:40}, 5, "up");
 	// ghost2 = new Ghost({x:200, y:200}, {w:40, h:40}, 5, "up");
