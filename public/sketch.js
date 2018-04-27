e215c66253c10f734d6f731dfde7f43e1f3570c7
keevitajamees <sven.suiste@gmail.com>
2018-04-27 22:12:57 +0300

    lower speed

--- public/sketch.js
+++ public/sketch.js
@@ -20,9 +20,9 @@ function setup() {
 	createCanvas(640, 480);
 	//const canvasElt = createCanvas(640, 480).elt;
   //canvasElt.style.width = '50%', canvasElt.style.height = '50%';
-  pac = new Pacman({x:100, y:100}, {w:40, h:40}, 5, "up");
+  pac = new Pacman({x:100, y:100}, {w:40, h:40}, 3, "up");
 	for(var i = 0; i < numOfGhosts; i++){
-		ghosts.push(new Ghost({x:200, y:200}, {w:20, h:20}, 4, "up"));
+		ghosts.push(new Ghost({x:200, y:200}, {w:20, h:20}, 2, "up"));
 	}
 	// ghost = new Ghost({x:200, y:200}, {w:40, h:40}, 5, "up");
 	// ghost2 = new Ghost({x:200, y:200}, {w:40, h:40}, 5, "up");
