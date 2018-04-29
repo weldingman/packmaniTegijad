d440df9f50570d091d5a411c1bff55a3747c7527
keevitajamees <sven.suiste@gmail.com>
2018-04-29 19:42:05 +0300

    add time and score

--- public/sketch.js
+++ public/sketch.js
@@ -5,12 +5,14 @@ var world;
 var ghost;
 var ghost2;
 var ghosts = [];
-var numOfGhosts = 6;
+var numOfGhosts = 2;
 var saveWorld = true;
 var loadWorld = true;
 let preWorld;
 var bg;
-var score = 0;
+var score = 100;
+var teime = 0;
+var startMillis;
 
 function preload(){
 	preWorld = loadJSON("world2.json");
@@ -18,12 +20,15 @@ function preload(){
 
 function setup() {
 	bg = loadImage("pngs/dreamWorld4.jpg");
+	//var myCanvas = createCanvas(640, 480);
 	createCanvas(640, 480);
+	startMillis = millis();
+	//myCanvas.parent('myContainer');
 	//const canvasElt = createCanvas(640, 480).elt;
-  //canvasElt.style.width = '50%', canvasElt.style.height = '50%';
-  pac = new Pacman({x:30, y:30}, {w:20, h:20}, 5, "up");
+  //canvasElt.style.width = '260px', canvasElt.style.height = "200px";
+  pac = new Pacman({x:30, y:30}, {w:20, h:20}, 2.5, "up");
 	for(var i = 0; i < numOfGhosts; i++){
-		ghosts.push(new Ghost({x:280, y:190}, {w:20, h:20}, 5, "up"));
+		ghosts.push(new Ghost({x:280, y:190}, {w:20, h:20}, 2, "up"));
 	}
   world = new TestWorld({w:20,h:20}, preWorld.wall, preWorld.branch, preWorld.pacTrace, preWorld.food);
 	world.setPactraceRandom();
@@ -33,23 +38,35 @@ function setup() {
 function draw() {
 	background(bg);
 	frameRate(60);
-	document.getElementById("mytext").value = score;
 	var worldObj = world.update(pac);
+	score += worldObj.addPoints;
+	time = ((millis() - (startMillis)) / 1000).toFixed(1);
+	strokeWeight(4);
+	stroke(51);
+	fill("yellow");
+	textSize(20);
+	textStyle(BOLD);
+	text("score: " + score, 150, 65);
+	text("time: " + time, 410, 65);
   pac.update(worldObj.wall);
 	for(var i = 0; i < ghosts.length; i++){
 		if(ghosts[i].update(worldObj, pac)){
 			restartWorld();
 		}
 	}
+	//document.getElementById("mytext").value = score;
 	lS();
+	//console.log(worldObj);
+	//console.log(document.getElementById("users").value);
+	//console.log(window.innerWidth);
 }
 
 function restartWorld(){
 	world.setPactraceRandom();
-	pac = new Pacman({x:30, y:30}, {w:20, h:20}, 5, "up");
+	pac = new Pacman({x:30, y:30}, {w:20, h:20}, 2.5, "up");
 	ghosts = [];
 	for(var i = 0; i < numOfGhosts; i++){
-		ghosts.push(new Ghost({x:280, y:190}, {w:20, h:20}, 5, "up"));
+		ghosts.push(new Ghost({x:280, y:190}, {w:20, h:20}, 2, "up"));
 	}
 }
 function lS(){
