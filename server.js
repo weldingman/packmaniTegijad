054170ffc43126388e34c4745ca60e1b811e354b
keevitajamees <sven.suiste@gmail.com>
2018-04-29 19:43:58 +0300

    add start page

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('pacmani veebileht'));
 
 app.listen(process.env.PORT || 8080, () => console.log('All is ok!'));
